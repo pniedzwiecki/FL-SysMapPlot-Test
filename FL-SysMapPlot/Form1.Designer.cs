f1e4b64422bfc0be9f3be7d55a955077ae153f8d
pniedzwiecki <pniedzwieckigm@gmail.com>
2024-02-04 00:32:09 -0500

    Add project files.

---
+++ FL-SysMapPlot/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace FL_SysMapPlot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelFolder = new System.Windows.Forms.Button();
+            this.btnSelFiles = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnProcessFiles = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.revDatePick = new System.Windows.Forms.DateTimePicker();
+            this.chkDefaultComment = new System.Windows.Forms.CheckBox();
+            this.chkAddRev = new System.Windows.Forms.CheckBox();
+            this.txtRevComment = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.rTxtInfo = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblFLC3DVersion = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelFolder
+            // 
+            this.btnSelFolder.Location = new System.Drawing.Point(14, 19);
+            this.btnSelFolder.Name = "btnSelFolder";
+            this.btnSelFolder.Size = new System.Drawing.Size(91, 23);
+            this.btnSelFolder.TabIndex = 0;
+            this.btnSelFolder.Text = "Select Folder";
+            this.btnSelFolder.UseVisualStyleBackColor = true;
+            this.btnSelFolder.Click += new System.EventHandler(this.btnSelFolder_Click);
+            // 
+            // btnSelFiles
+            // 
+            this.btnSelFiles.Location = new System.Drawing.Point(260, 19);
+            this.btnSelFiles.Name = "btnSelFiles";
+            this.btnSelFiles.Size = new System.Drawing.Size(91, 23);
+            this.btnSelFiles.TabIndex = 1;
+            this.btnSelFiles.Text = "Select Files";
+            this.btnSelFiles.UseVisualStyleBackColor = true;
+            this.btnSelFiles.Click += new System.EventHandler(this.btnSelFiles_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(207, 217);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnProcessFiles
+            // 
+            this.btnProcessFiles.Location = new System.Drawing.Point(111, 217);
+            this.btnProcessFiles.Name = "btnProcessFiles";
+            this.btnProcessFiles.Size = new System.Drawing.Size(90, 23);
+            this.btnProcessFiles.TabIndex = 3;
+            this.btnProcessFiles.Text = "Process Files";
+            this.btnProcessFiles.UseVisualStyleBackColor = true;
+            this.btnProcessFiles.Click += new System.EventHandler(this.btnProcessFiles_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.revDatePick);
+            this.groupBox1.Controls.Add(this.chkDefaultComment);
+            this.groupBox1.Controls.Add(this.chkAddRev);
+            this.groupBox1.Controls.Add(this.txtRevComment);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(17, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 135);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Revision Options";
+            // 
+            // revDatePick
+            // 
+            this.revDatePick.CustomFormat = "MM/dd/yyyy";
+            this.revDatePick.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.revDatePick.Location = new System.Drawing.Point(112, 42);
+            this.revDatePick.Name = "revDatePick";
+            this.revDatePick.Size = new System.Drawing.Size(116, 20);
+            this.revDatePick.TabIndex = 18;
+            this.revDatePick.ValueChanged += new System.EventHandler(this.revDatePick_ValueChanged);
+            // 
+            // chkDefaultComment
+            // 
+            this.chkDefaultComment.AutoSize = true;
+            this.chkDefaultComment.Location = new System.Drawing.Point(14, 82);
+            this.chkDefaultComment.Name = "chkDefaultComment";
+            this.chkDefaultComment.Size = new System.Drawing.Size(107, 17);
+            this.chkDefaultComment.TabIndex = 17;
+            this.chkDefaultComment.Text = "Default Comment";
+            this.chkDefaultComment.UseVisualStyleBackColor = true;
+            this.chkDefaultComment.CheckedChanged += new System.EventHandler(this.chkDefaultComment_CheckedChanged);
+            // 
+            // chkAddRev
+            // 
+            this.chkAddRev.AutoSize = true;
+            this.chkAddRev.Location = new System.Drawing.Point(14, 19);
+            this.chkAddRev.Name = "chkAddRev";
+            this.chkAddRev.Size = new System.Drawing.Size(89, 17);
+            this.chkAddRev.TabIndex = 15;
+            this.chkAddRev.Text = "Add Revision";
+            this.chkAddRev.UseVisualStyleBackColor = true;
+            this.chkAddRev.CheckedChanged += new System.EventHandler(this.chkAddRev_CheckedChanged);
+            // 
+            // txtRevComment
+            // 
+            this.txtRevComment.Location = new System.Drawing.Point(112, 105);
+            this.txtRevComment.Name = "txtRevComment";
+            this.txtRevComment.Size = new System.Drawing.Size(239, 20);
+            this.txtRevComment.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Revision Comment:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Revision Date:";
+            // 
+            // rTxtInfo
+            // 
+            this.rTxtInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rTxtInfo.Location = new System.Drawing.Point(12, 246);
+            this.rTxtInfo.Name = "rTxtInfo";
+            this.rTxtInfo.ReadOnly = true;
+            this.rTxtInfo.Size = new System.Drawing.Size(368, 237);
+            this.rTxtInfo.TabIndex = 7;
+            this.rTxtInfo.Text = "";
+            this.rTxtInfo.WordWrap = false;
+            this.rTxtInfo.TextChanged += new System.EventHandler(this.rTxtInfo_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSelFiles);
+            this.groupBox2.Controls.Add(this.btnSelFolder);
+            this.groupBox2.Location = new System.Drawing.Point(17, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(363, 58);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select Folder/Files";
+            // 
+            // lblFLC3DVersion
+            // 
+            this.lblFLC3DVersion.AutoSize = true;
+            this.lblFLC3DVersion.Location = new System.Drawing.Point(14, 490);
+            this.lblFLC3DVersion.Name = "lblFLC3DVersion";
+            this.lblFLC3DVersion.Size = new System.Drawing.Size(56, 13);
+            this.lblFLC3DVersion.TabIndex = 13;
+            this.lblFLC3DVersion.Text = "WadeTrim";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Location = new System.Drawing.Point(261, 490);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(119, 13);
+            this.lblVersion.TabIndex = 14;
+            this.lblVersion.Text = "Version 2.0";
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(392, 510);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblFLC3DVersion);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.rTxtInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnProcessFiles);
+            this.Controls.Add(this.btnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Ford Land System Map Plot Utility";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelFolder;
+        private System.Windows.Forms.Button btnSelFiles;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnProcessFiles;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtRevComment;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox chkAddRev;
+        private System.Windows.Forms.CheckBox chkDefaultComment;
+        private System.Windows.Forms.RichTextBox rTxtInfo;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker revDatePick;
+        private System.Windows.Forms.Label lblFLC3DVersion;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FL-SysMapPlot/Form1.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace FL_SysMapPlot
+{
+    public partial class Form1 : Form
+    {
+        string revDefaultComment = "INFRASTRUCTURE COORDINATION";
+        string revDate;
+        int dwgPerCore;
+        int coreOne, coreTwo, coreThree, coreFour, dwgExtra;
+        string path;
+        string[] filePaths;
+        //                               C:\offnetworkcadd\WadeTrim\Autodesk\Ford
+        const string C3D_VersionPath = @"C:\offnetworkcadd\WadeTrim\Autodesk\Ford\FordLandVersion.txt";
+        string version = "";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            chkAddRev.Checked = false;
+            txtRevComment.Enabled = false;
+            chkDefaultComment.Enabled = false;
+            btnSelFiles.Enabled = false;
+            revDatePick.Enabled = false;
+            btnProcessFiles.Enabled = false;
+            lblVersion.Text = "Version " + typeof(Form1).Assembly.GetName().Version.ToString();
+            version = GetFLC3DVersion(C3D_VersionPath);
+            //Check to see if the FordLandVersion.txt file exists and if the version
+            //listed in the text file is installed, if both are true then continue else
+            //display a message box and then exit.
+            if (File.Exists(C3D_VersionPath))
+            {
+                string appPath = "C:\\Program Files\\Autodesk\\AutoCAD " + version;
+                if (Directory.Exists(appPath))
+                {
+                    lblFLC3DVersion.Text = "WadeTrim C3D " + version;
+                }
+                else
+                {
+                    MessageBox.Show("The required version of Civil 3D is not installed on this pc." +
+                        "\nContact IT for assistance.");
+                    //this.Close();
+                    btnSelFolder.Enabled = false;
+                    chkAddRev.Enabled = false;
+                    lblFLC3DVersion.Text = "WadeTrim";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Missing FordLandVersion.txt file." +
+                    "\nContact IT for assistance.");
+                //this.Close();
+                btnSelFolder.Enabled = false;
+                chkAddRev.Enabled = false;
+                lblFLC3DVersion.Text = "WadeTrim";
+            }
+        }
+
+        private void btnSelFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowserDialog1.SelectedPath;
+                filePaths = Directory.GetFiles(path, "*.dwg");
+                DisplayInfo(filePaths, path);
+            }
+            btnSelFiles.Enabled = false;
+        }
+
+        private void DisplayInfo(string[] filePaths, string path)
+        {
+            if (filePaths.Length == 0)
+            {
+                MessageBox.Show("No drawing files found in selected folder!" +
+                    "\nSelect another folder.", "No drawings found!");
+            }
+            else
+            {
+                int cntr = 0;
+
+                foreach (string file in filePaths)
+                {
+                    rTxtInfo.AppendText(file + Environment.NewLine);
+                    cntr++;
+                }
+
+                rTxtInfo.AppendText("\n" + filePaths.Length.ToString() + " drawings files.\n");
+
+                if ((cntr % 4) == 0)
+                {
+                    dwgPerCore = cntr / 4;
+                    coreOne = dwgPerCore;
+                    coreTwo = dwgPerCore;
+                    coreThree = dwgPerCore;
+                    coreFour = dwgPerCore;
+                }
+                else
+                {
+                    dwgExtra = cntr % 4;
+                    dwgPerCore = (cntr - dwgExtra) / 4;
+
+                    if (dwgExtra == 3)
+                    {
+                        coreOne = dwgPerCore + 1;
+                        coreTwo = dwgPerCore + 1;
+                        coreThree = dwgPerCore + 1;
+                        coreFour = dwgPerCore;
+                    }
+                    else if (dwgExtra == 2)
+                    {
+                        coreOne = dwgPerCore + 1;
+                        coreTwo = dwgPerCore + 1;
+                        coreThree = dwgPerCore;
+                        coreFour = dwgPerCore;
+                    }
+                    else
+                    {
+                        coreOne = dwgPerCore + 1;
+                        coreTwo = dwgPerCore;
+                        coreThree = dwgPerCore;
+                        coreFour = dwgPerCore;
+                    }
+                }
+
+                rTxtInfo.AppendText("\nDrawings per core (+/-) = " + dwgPerCore.ToString());
+                rTxtInfo.AppendText("\n" + coreOne + " files in core 1");
+                rTxtInfo.AppendText("\n" + coreTwo + " files in core 2");
+                rTxtInfo.AppendText("\n" + coreThree + " files in core 3");
+                rTxtInfo.AppendText("\n" + coreFour + " files in core 4\n");
+
+                btnProcessFiles.Enabled = true;
+            }
+        }
+
+        private void WriteBatchFiles(string[] filePaths, int core, int count, string corName, string path)
+        {
+            StreamWriter batchOut = new StreamWriter(path + "\\" + corName, append: true);
+
+            int increment = count - core;
+
+            while (increment < count)
+            {
+                batchOut.WriteLine("\"C:\\Program Files\\Autodesk\\AutoCAD " + version + "\\accoreconsole.exe\"" + " /i "
+                    + "\"" + filePaths[increment] + "\""
+                    + " /s "
+                    + "\"C:\\offnetworkcadd\\WadeTrim\\Autodesk\\Support\\Lisp\\CAD Management\\FL-SysMapPlot.scr\""
+                    + " /l en-US");
+                increment++;
+            }
+
+            batchOut.WriteLine("exit");
+
+            batchOut.Close();
+        }
+
+        private string GetFLC3DVersion(string path)
+        {
+            string version;
+
+            using (StreamReader versionTxt = new StreamReader(path))
+            {
+                version = versionTxt.ReadLine();
+            }
+
+            return version;
+        }
+
+        private void btnSelFiles_Click(object sender, EventArgs e)
+        {
+            btnSelFolder.Enabled = false;
+        }
+
+        private void chkAddRev_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAddRev.Checked == true)
+            {
+                txtRevComment.Enabled = true;
+                chkDefaultComment.Enabled = true;
+                chkDefaultComment.Checked = true;
+                txtRevComment.Text = revDefaultComment;
+                revDatePick.Enabled = true;
+            }
+            else if (chkAddRev.Checked == false)
+            {
+                chkDefaultComment.Enabled = false;
+                chkDefaultComment.Checked = false;
+                chkDefaultComment.Enabled = false;
+                txtRevComment.Text = "";
+                txtRevComment.Enabled = false;
+                revDatePick.Enabled = false;
+
+            }
+        }
+
+        private void btnProcessFiles_Click(object sender, EventArgs e)
+        {
+            rTxtInfo.Clear();
+            rTxtInfo.AppendText("Processing files...");
+
+            WriteBatchFiles(filePaths, coreOne, coreOne, "CoreOne.bat", path);
+            WriteBatchFiles(filePaths, coreTwo, (coreOne + coreTwo), "CoreTwo.bat", path);
+            WriteBatchFiles(filePaths, coreThree, (coreOne + coreTwo + coreThree), "CoreThree.bat", path);
+            WriteBatchFiles(filePaths, coreFour, (coreOne + coreTwo + coreThree + coreFour), "CoreFour.bat", path);
+
+            if (chkAddRev.Checked == true)
+            {
+                StreamWriter revInfo = new StreamWriter(path + "\\revInfo.rev");
+                revInfo.WriteLine(revDate = revDatePick.Value.ToString("MM/dd/yyyy"));
+                if (chkDefaultComment.Checked == true)
+                {
+                    revInfo.WriteLine(revDefaultComment);
+                }
+                else
+                {
+                    revInfo.WriteLine(txtRevComment.Text);
+                }
+
+                revInfo.Close();
+
+            }
+
+            ProcessStartInfo CoreOne;
+            ProcessStartInfo CoreTwo;
+            ProcessStartInfo CoreThree;
+            ProcessStartInfo CoreFour;
+
+            Process ProcCoreOne;
+            Process ProcCoreTwo;
+            Process ProcCoreThree;
+            Process ProcCoreFour;
+
+            CoreOne = new ProcessStartInfo("cmd.exe", @"/c " + path + "\\CoreOne.bat")
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            CoreTwo = new ProcessStartInfo("cmd.exe", @"/c " + path + "\\CoreTwo.bat")
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            CoreThree = new ProcessStartInfo("cmd.exe", @"/c " + path + "\\CoreThree.bat")
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            CoreFour = new ProcessStartInfo("cmd.exe", @"/c " + path + "\\CoreFour.bat")
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            ProcCoreOne = Process.Start(CoreOne);
+            ProcCoreTwo = Process.Start(CoreTwo);
+            ProcCoreThree = Process.Start(CoreThree);
+            ProcCoreFour = Process.Start(CoreFour);
+
+            ProcCoreOne.WaitForExit();
+            ProcCoreTwo.WaitForExit();
+            ProcCoreThree.WaitForExit();
+            ProcCoreFour.WaitForExit();
+
+            ProcCoreOne.Close();
+            ProcCoreTwo.Close();
+            ProcCoreThree.Close();
+            ProcCoreFour.Close();
+
+            MessageBox.Show("Process Complete", "Ford Land System Map Plots");
+
+            VerifyPlots();
+
+            CleanUpFiles();
+
+        }
+
+        private void CountPdfs()
+        {
+            string[] pdfPaths = Directory.GetFiles(path, "*.pdf");
+
+            if (pdfPaths.Length == filePaths.Length)
+            {
+                rTxtInfo.Clear();
+                rTxtInfo.AppendText("All drawings were plotted to pdf.");
+            }
+            else if ((filePaths.Length - pdfPaths.Length) == 1)
+            {
+                int missing = (filePaths.Length - pdfPaths.Length);
+                rTxtInfo.Clear();
+                rTxtInfo.AppendText(missing.ToString() + " drawing did not plot to pdf.");
+                //VerifyPlots();
+            }
+            else
+            {
+                int missing = (filePaths.Length - pdfPaths.Length);
+                rTxtInfo.Clear();
+                rTxtInfo.AppendText(missing.ToString() + " drawings did not plot to pdf.");
+                //VerifyPlots();
+            }
+        }
+
+        private void CleanUpFiles()
+        {
+            if (File.Exists(path + "\\CoreOne.bat"))
+            {
+                File.Delete(path + "\\CoreOne.bat");
+            }
+            if (File.Exists(path + "\\CoreTwo.bat"))
+            {
+                File.Delete(path + "\\CoreTwo.bat");
+            }
+            if (File.Exists(path + "\\CoreThree.bat"))
+            {
+                File.Delete(path + "\\CoreThree.bat");
+            }
+            if (File.Exists(path + "\\CoreFour.bat"))
+            {
+                File.Delete(path + "\\CoreFour.bat");
+            }
+            if (File.Exists(path + "\\revInfo.rev"))
+            {
+                File.Delete(path + "\\revInfo.rev");
+            }
+        }
+
+        private void VerifyPlots()
+        {
+            rTxtInfo.Clear();
+            string dwgName;
+            string pdfName;
+            int position;
+            string fullPath;
+
+            string[] pdfPaths = Directory.GetFiles(path, "*.pdf");
+
+            foreach (string file in filePaths)
+            {
+                fullPath = file;
+                position = fullPath.LastIndexOf('\\');
+                dwgName = fullPath.Substring(position + 1);
+                pdfName = dwgName.Substring(0, dwgName.Length - 4) + "*.pdf";
+                bool fileExist = Directory.EnumerateFiles(path, pdfName).Any();
+
+                if (!fileExist)
+                {
+                    rTxtInfo.AppendText(dwgName + " was not plotted to pdf!\n");
+                }
+            }
+        }
+
+        private void revDatePick_ValueChanged(object sender, EventArgs e)
+        {
+            revDate = revDatePick.Value.ToString("MM/dd/yyyy");
+        }
+
+        private void chkDefaultComment_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkDefaultComment.Checked == true)
+            {
+                txtRevComment.Enabled = false;
+                txtRevComment.Text = revDefaultComment;
+            }
+            else if (chkDefaultComment.Checked == false)
+            {
+                txtRevComment.Enabled = true;
+                txtRevComment.Text = "";
+                txtRevComment.Focus();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rTxtInfo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
